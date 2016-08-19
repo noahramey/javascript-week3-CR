c598fdc6ac46d4fdbe992fc256aa7828331fccbd
Noah Ramey <noahramey1997@gmail.com>
2016-08-19 10:06:37 -0700

    add bootstrap styling, about and contact pages, and edit the README.md

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,19 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  addNewQuestion: false,
   actions: {
-    questionFormShow() {
-      this.set('addNewQuestion', true);
-    },
-
     save() {
       var params = {
         author: this.get('author'),
         question: this.get('question'),
         notes: this.get('notes'),
       };
-      this.set('addNewQuestion', false);
       this.sendAction('save', params);
     }
   }
